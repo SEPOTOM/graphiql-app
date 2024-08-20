11a7f3132d111e918e8df71099a4eb58c8951254
Artyom Pizin <artempetr2016@gmail.com>
2024-08-20 12:08:19 +0300

    init: scaffold the project with create-next-app

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import { Inter } from "next/font/google";
+
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+const  RootLayout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
+
+export default RootLayout;
