ce5a6fcb9b33ef405fab6d0a5efb12a146d0d1f7
Artyom Pizin <artempetr2016@gmail.com>
2024-08-23 15:19:33 +0300

    feat(SignUpForm): control form with React Hook Form

--- src/components/SignUpForm/SignUpForm.tsx
+++ src/components/SignUpForm/SignUpForm.tsx
@@ -1,25 +1,30 @@
 'use client';
 
+import { SubmitHandler, useForm } from 'react-hook-form';
 import { Box, Button, TextField, Typography } from '@mui/material';
 
+import { SignUpData } from '@/components/SignUpForm/types';
+
 const SignUpForm = () => {
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
+  const { handleSubmit, register } = useForm<SignUpData>();
+
+  const onSubmit: SubmitHandler<SignUpData> = (data) => {
+    console.log(data);
   };
 
   return (
     <Box
       component="form"
-      onSubmit={handleSubmit}
+      onSubmit={handleSubmit(onSubmit)}
       sx={{ display: 'flex', flexDirection: 'column', gap: 2, width: '100%' }}
     >
       <Typography variant="h3" component="h1" gutterBottom>
         Sign Up
       </Typography>
-      <TextField label="Username" name="username" required />
-      <TextField label="Email" name="email" type="email" required />
-      <TextField label="Password" name="password" type="password" required />
-      <TextField label="Confirm Password" name="confirmPassword" type="password" required />
+      <TextField label="Username" {...register('username')} required />
+      <TextField label="Email" {...register('email')} type="email" required />
+      <TextField label="Password" {...register('password')} type="password" required />
+      <TextField label="Confirm Password" {...register('confirmPassword')} type="password" required />
       <Button variant="contained" color="primary" type="submit">
         Sign Up
       </Button>
