aa564e36a5f3bc3283ea1ec40f79aff6aec90711
Artyom Pizin <artempetr2016@gmail.com>
2024-08-25 20:13:31 +0300

    feat(auth): use all env variables for firebase services

--- src/app/sign-up/api/route.ts
+++ src/app/sign-up/api/route.ts
@@ -3,16 +3,24 @@ import * as admin from 'firebase-admin';
 
 import { SignUpData, TokenRes } from '@/types';
 
-const serviceAccount: admin.ServiceAccount = {
-  projectId: process.env.ADMIN_FIREBASE_PROJECT_ID,
-  clientEmail: process.env.ADMIN_FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.ADMIN_FIREBASE_PRIVATE_KEY,
+const serviceAccount = {
+  type: process.env.ADMIN_FIREBASE_TYPE,
+  project_id: process.env.ADMIN_FIREBASE_PROJECT_ID,
+  private_key_id: process.env.ADMIN_FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.ADMIN_FIREBASE_PRIVATE_KEY,
+  client_email: process.env.ADMIN_FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.ADMIN_FIREBASE_CLIENT_ID,
+  auth_uri: process.env.ADMIN_FIREBASE_AUTH_URI,
+  token_uri: process.env.ADMIN_FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.ADMIN_FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.ADMIN_FIREBASE_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.ADMIN_FIREBASE_UNIVERSE_DOMAIN,
 };
 
 const getFirebaseApp = () => {
   if (admin.apps.length === 0) {
     return admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
+      credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
     });
   } else {
     return admin.app();
