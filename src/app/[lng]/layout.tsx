5afd73b79e569fa922dbd5ac01db6d5c2d055777
Artyom Pizin <artempetr2016@gmail.com>
2024-08-27 13:30:53 +0300

    feat: move root layout to the lng route

--- src/app/[lng]/layout.tsx
+++ src/app/[lng]/layout.tsx
@@ -1,13 +1,20 @@
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
 import CssBaseline from '@mui/material/CssBaseline';
+import { dir } from 'i18next';
+
+import { LngParam } from '@/types';
 
 const RootLayout = ({
   children,
+  params,
 }: Readonly<{
   children: React.ReactNode;
+  params: LngParam;
 }>) => {
+  const { lng } = params;
+
   return (
-    <html lang="en">
+    <html lang={lng} dir={dir(lng)}>
       <body>
         <CssBaseline />
         <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
