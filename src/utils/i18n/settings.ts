fdd52bc9040e958ca7f109fadf9bc0ba9e780d45
Artyom Pizin <artempetr2016@gmail.com>
2024-08-27 13:45:00 +0300

    feat(i18n): add languages and cookie name constants

---
+++ src/utils/i18n/settings.ts
@@ -0,0 +1,3 @@
+export const fallbackLng = 'en';
+export const languages = [fallbackLng, 'ru'];
+export const i18nCookieName = 'i18next';
---
+++ src/utils/index.ts
@@ -0,0 +1,3 @@
+import { fallbackLng, i18nCookieName, languages } from '@/utils/i18n/settings';
+
+export { fallbackLng, languages, i18nCookieName };
