cfbb90a02dc36256247f3ae8571d75d869b18392
Svetlana <svetailina1987@gmail.com>
2024-08-27 18:12:23 +0300

    test: add tests for RestfullClient

---
+++ src/components/restfullClient/restfullClient.test.tsx
@@ -0,0 +1,34 @@
+import { render } from '@testing-library/react';
+import { usePathname, useRouter } from 'next/navigation';
+import { Mock } from 'vitest';
+import RestfullClient from './restfullClient';
+
+const replace = vi.fn();
+(useRouter as Mock).mockImplementation(() => ({
+  replace,
+}));
+
+afterEach(() => {
+  replace.mockRestore();
+});
+
+describe('RestfullClient component', () => {
+  it('should set the method to GET if the URL has no method', () => {
+    (usePathname as Mock).mockReturnValue('/restfullClient');
+    render(<RestfullClient />);
+    expect(replace).toHaveBeenCalledWith('/restfullClient/GET');
+  });
+  it('should replace an invalid ыупьуте with GET', () => {
+    const encodedSegment = btoa('qwerty');
+    (usePathname as Mock).mockReturnValue(`/restfullClient/${encodedSegment}`);
+
+    render(<RestfullClient />);
+    expect(replace).toHaveBeenCalledWith(`/restfullClient/GET/${encodedSegment}`);
+  });
+  it('should not change the URL if a valid method is present', () => {
+    (usePathname as Mock).mockReturnValue('/restfullClient/GET');
+
+    render(<RestfullClient />);
+    expect(replace).not.toHaveBeenCalled();
+  });
+});
--- src/tests/setupTests.ts
+++ src/tests/setupTests.ts
@@ -5,6 +5,7 @@ import { server } from '@/tests/mocks/server';
 beforeAll(() => {
   vi.mock('next/navigation', () => ({
     usePathname: vi.fn(),
+    useRouter: vi.fn(),
   }));
   server.listen();
 });
