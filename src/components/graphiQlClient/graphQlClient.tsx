882567c18c1a93322303bb13f4dea5e1c2376f3d
Pavel Kuvshinov <pavelkuvsh1noff@mail.ru>
2024-08-30 12:11:25 +0300

    fix: Remove unnessasery code.

--- src/components/graphiQlClient/graphQlClient.tsx
+++ src/components/graphiQlClient/graphQlClient.tsx
@@ -3,21 +3,8 @@
 import { Box } from '@mui/material';
 import MenuTab from './menuTab/MenuTab';
 import EndpointsForm from './endpointsForm/EndpointsForm';
-import { usePathname, useRouter } from 'next/navigation';
-import { useEffect } from 'react';
-import { makeGraphQLRequest } from '@/services/makeGraphQlRequests';
-import { graphQLSchemaQuery, headersGraphQLSchema } from '@/constants/constants';
 
 export default function GraphQlClient() {
-  const pathname = usePathname();
-  const currentEndpoint = pathname.split('/').splice(2).join('/') || '';
-  const router = useRouter();
-
-  useEffect(() => {
-    router.replace(pathname);
-    makeGraphQLRequest(graphQLSchemaQuery, atob(currentEndpoint), headersGraphQLSchema);
-  }, []);
-
   return (
     <Box
       sx={{
