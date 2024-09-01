379c492541c08920718b925913ee3c5cb7a565ef
Artyom Pizin <artempetr2016@gmail.com>
2024-09-01 14:59:10 +0300

    refactor: format files

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,5 @@
-
 import RestfullClient from './restfullClient/RestfullClient';
 import MUIThemeProvider from '@/components/MUIThemeProvider/MUIThemeProvider';
 import Footer from '@/components/Footer/Footer';
 
 export { MUIThemeProvider, RestfullClient, Footer };
-
