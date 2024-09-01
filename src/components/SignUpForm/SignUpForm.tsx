eccf0885f695ccaedc77415b1587376251a1a732
Artyom Pizin <artempetr2016@gmail.com>
2024-09-01 19:52:05 +0300

    feat(SignUpForm): display a message after successful registration

--- src/components/SignUpForm/SignUpForm.tsx
+++ src/components/SignUpForm/SignUpForm.tsx
@@ -1,15 +1,18 @@
 'use client';
 
+import { useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { Box, Button, TextField, Typography } from '@mui/material';
 
 import { useAuth } from '@/contexts';
+import { Notification } from '@/components';
 
 import { SignUpFormData } from './types';
 
 const SignUpForm = () => {
   const { signUp } = useAuth();
   const { handleSubmit, register } = useForm<SignUpFormData>();
+  const [isSuccess, setIsSuccess] = useState(false);
 
   const onSubmit: SubmitHandler<SignUpFormData> = async ({ username, email, password }) => {
     await signUp({
@@ -17,25 +20,32 @@ const SignUpForm = () => {
       password,
       displayName: username,
     });
+
+    setIsSuccess(true);
   };
 
   return (
-    <Box
-      component="form"
-      onSubmit={handleSubmit(onSubmit)}
-      sx={{ display: 'flex', flexDirection: 'column', gap: 2, width: '100%' }}
-    >
-      <Typography variant="h3" component="h1" gutterBottom>
-        Sign Up
-      </Typography>
-      <TextField label="Username" {...register('username')} required />
-      <TextField label="Email" {...register('email')} type="email" required />
-      <TextField label="Password" {...register('password')} type="password" required />
-      <TextField label="Confirm Password" {...register('confirmPassword')} type="password" required />
-      <Button variant="contained" color="primary" type="submit">
-        Sign Up
-      </Button>
-    </Box>
+    <>
+      <Box
+        component="form"
+        onSubmit={handleSubmit(onSubmit)}
+        sx={{ display: 'flex', flexDirection: 'column', gap: 2, width: '100%' }}
+      >
+        <Typography variant="h3" component="h1" gutterBottom>
+          Sign Up
+        </Typography>
+        <TextField label="Username" {...register('username')} required />
+        <TextField label="Email" {...register('email')} type="email" required />
+        <TextField label="Password" {...register('password')} type="password" required />
+        <TextField label="Confirm Password" {...register('confirmPassword')} type="password" required />
+        <Button variant="contained" color="primary" type="submit">
+          Sign Up
+        </Button>
+      </Box>
+      <Notification open={isSuccess} onClose={() => setIsSuccess(false)}>
+        Successfully signed up
+      </Notification>
+    </>
   );
 };
 
