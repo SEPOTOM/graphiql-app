aedd80af67a0a17158a8fa56c995ef323824a88c
Artyom Pizin <artempetr2016@gmail.com>
2024-09-02 15:50:19 +0300

    test: mock sign up route handler

--- src/tests/mocks/handlers.ts
+++ src/tests/mocks/handlers.ts
@@ -1,3 +1,13 @@
-import { HttpHandler } from 'msw';
+import { HttpHandler, HttpResponse, http } from 'msw';
 
-export const handlers: HttpHandler[] = [];
+import { TokenRes } from '@/types';
+
+export const handlers: HttpHandler[] = [
+  http.post('/sign-up/api', () => {
+    const resBody: TokenRes = {
+      token: 'test-token',
+    };
+
+    return HttpResponse.json(resBody);
+  }),
+];
