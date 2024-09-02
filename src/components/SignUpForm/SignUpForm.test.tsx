bc94e1d014ebe077a92b4a2fd60c22d903789172
Artyom Pizin <artempetr2016@gmail.com>
2024-09-02 15:53:17 +0300

    test(SignUpForm): display success alert after successful registration

--- src/components/SignUpForm/SignUpForm.test.tsx
+++ src/components/SignUpForm/SignUpForm.test.tsx
@@ -1,5 +1,8 @@
 import { render } from '@testing-library/react';
 
+import { renderWithUser } from '@/tests';
+import { AuthProvider } from '@/contexts';
+
 import SignUpForm from './SignUpForm';
 
 describe('SignUpForm', () => {
@@ -11,5 +14,21 @@ describe('SignUpForm', () => {
     expect(getByRole('button', { name: /sign up/i })).toBeInTheDocument();
     expect(getByLabelText(/^password/i)).toBeInTheDocument();
     expect(getByLabelText(/confirm password/i)).toBeInTheDocument();
+  });
+
+  it('displays success alert after successful registration', async () => {
+    const { user, getByRole, getByLabelText, findByRole } = renderWithUser(
+      <AuthProvider>
+        <SignUpForm lng="en" />
+      </AuthProvider>
+    );
+
+    await user.type(getByRole('textbox', { name: /username/i }), 'Mark');
+    await user.type(getByRole('textbox', { name: /email/i }), 'mark@email.com');
+    await user.type(getByLabelText(/^password/i), 'mark12345678');
+    await user.type(getByLabelText(/confirm password/i), 'mark12345678');
+    await user.click(getByRole('button', { name: /sign up/i }));
+
+    expect(await findByRole('alert')).toHaveTextContent(/success/i);
   });
 });
