b887f39dc42207db5f9237b947fcc74b216e48e5
Pavel Kuvshinov <pavelkuvsh1noff@mail.ru>
2024-09-03 13:52:08 +0300

    feat: Add table headings translations. Add graphQL context.

--- src/components/graphQlClient/menuTab/MenuTabPanel.tsx
+++ src/components/graphQlClient/menuTab/MenuTabPanel.tsx
@@ -1,6 +1,11 @@
 'use client';
 
+import EditorTable from '@/components/EditorTable/EditorTable';
+import { GraphQlDataContext } from '@/contexts/GraphQLContext/GraphQLContext';
+import { DataItem } from '@/contexts/GraphQLContext/types';
+import { GraphQlHeadersEditor, GraphQlVariablesEditor } from '@/types/enum';
 import { Box } from '@mui/material';
+import { SetStateAction, useContext } from 'react';
 
 interface TabPanelProps {
   children?: React.ReactNode;
@@ -11,15 +16,28 @@ interface TabPanelProps {
 
 export default function CustomTabPanel(props: TabPanelProps) {
   const { children, value, index, content, ...other } = props;
+
+  const { paramData, setParamData, headerData, setHeaderData } = useContext(GraphQlDataContext);
+  const headerEditors = Object.values(GraphQlHeadersEditor) as string[];
+  const variablesEditors = Object.values(GraphQlVariablesEditor) as string[];
+  const editors = headerEditors.concat(variablesEditors);
+
   return (
     <Box
+      width="100%"
       role="tabpanel"
       hidden={value !== index}
       id={`simple-tabpanel-${index}`}
       aria-labelledby={`simple-tab-${index}`}
       {...other}
     >
-      <p>{content} will be here ...</p>
+      {editors.includes(content) ?
+        <EditorTable
+          heading={content}
+          data={variablesEditors.includes(content) ? paramData : headerData}
+          setData={variablesEditors.includes(content) ? setParamData : setHeaderData}
+        />
+      : <p>Here not be editor</p>}
     </Box>
   );
 }
--- src/types/enum.ts
+++ src/types/enum.ts
@@ -4,6 +4,16 @@ export enum GraphQlMenuTabs {
   Schema = 'graphQllMenuTaSchema',
 }
 
+export enum GraphQlHeadersEditor {
+  HeadersEditorEN = 'Headers',
+  HeadersEditorRU = 'Заголовки',
+}
+
+export enum GraphQlVariablesEditor {
+  HeadersEditorEN = 'Variables',
+  HeadersEditorRU = 'Переменные',
+}
+
 export enum Method {
   Get = 'GET',
   Post = 'POST',
