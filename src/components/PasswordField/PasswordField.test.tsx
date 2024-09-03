a519af8846e66ae5a99481a740996c99e9284e41
Artyom Pizin <artempetr2016@gmail.com>
2024-09-03 23:05:24 +0300

    test(PasswordField): verify password visibility on toggle

--- src/components/PasswordField/PasswordField.test.tsx
+++ src/components/PasswordField/PasswordField.test.tsx
@@ -1,5 +1,7 @@
 import { render } from '@testing-library/react';
 
+import { renderWithUser } from '@/tests';
+
 import PasswordField from './PasswordField';
 
 describe('PasswordField', () => {
@@ -7,5 +9,13 @@ describe('PasswordField', () => {
     const { findByRole } = render(<PasswordField lng="en" />);
 
     expect(await findByRole('button', { name: /show/i }));
+  });
+
+  it('shows the password by pressing the toggle button', async () => {
+    const { user, findByRole, getByLabelText } = renderWithUser(<PasswordField lng="en" label="password" />);
+
+    await user.click(await findByRole('button', { name: /show/i }));
+
+    expect(getByLabelText('password')).toHaveAttribute('type', 'text');
   });
 });
