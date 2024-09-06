83e2c433fbae65df7ab6c5f0cfcb9ed8cdc55c3f
Artyom Pizin <artempetr2016@gmail.com>
2024-09-06 23:21:00 +0300

    feat(SignInForm): register form with react-hook-form

--- src/components/SignInForm/SignInForm.tsx
+++ src/components/SignInForm/SignInForm.tsx
@@ -1,23 +1,35 @@
 'use client';
 
 import { Box, Button, TextField, Typography, useMediaQuery, useTheme } from '@mui/material';
+import { SubmitHandler, useForm } from 'react-hook-form';
 
 import { PasswordField } from '@/components';
+
+import { SignInFormData } from './types';
 
 const SignInForm = () => {
   const theme = useTheme();
   const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
+  const { handleSubmit, register } = useForm<SignInFormData>();
+
+  const onSubmit: SubmitHandler<SignInFormData> = (data) => {
+    console.log(data);
+  };
 
   const inputSize = isSmallScreen ? 'small' : 'medium';
 
   return (
-    <Box component="form" sx={{ display: 'flex', flexDirection: 'column', gap: 2, width: '100%' }}>
+    <Box
+      component="form"
+      onSubmit={handleSubmit(onSubmit)}
+      sx={{ display: 'flex', flexDirection: 'column', gap: 2, width: '100%' }}
+    >
       <Typography variant={isSmallScreen ? 'h3' : 'h2'} component="h1" gutterBottom>
         Sign In
       </Typography>
 
-      <TextField label="Email" name="email" required size={inputSize} />
-      <PasswordField label="Password" name="password" required lng="en" size={inputSize} />
+      <TextField label="Email" {...register('email')} required size={inputSize} />
+      <PasswordField label="Password" {...register('password')} required lng="en" size={inputSize} />
 
       <Button variant="contained" color="primary" type="submit">
         Sign In
