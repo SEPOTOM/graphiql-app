9e86c1d1e7595a2ec04d4599f41f126c3fca3bab
Artyom Pizin <artempetr2016@gmail.com>
2024-09-07 14:57:36 +0300

    test(SignInForm): add test for disabling form during submit

--- src/components/SignInForm/SignInForm.test.tsx
+++ src/components/SignInForm/SignInForm.test.tsx
@@ -26,4 +26,24 @@ describe('SignInForm', () => {
 
     expect(await findByRole('alert')).toHaveTextContent(/success/i);
   });
+
+  it('disables form widgets while the form is submitting', async () => {
+    const { user, getByRole, getByLabelText, getAllByRole } = renderWithUser(
+      <AuthProvider>
+        <SignInForm lng="en" />
+      </AuthProvider>
+    );
+    const emailInput = getByRole('textbox', { name: /email/i });
+    const pwdInput = getByLabelText(/^password/i);
+
+    await user.type(emailInput, 'mark@email.com');
+    await user.type(pwdInput, 'mark12345678');
+    await user.click(getByRole('button', { name: /sign in/i }));
+
+    expect(emailInput).toBeDisabled();
+    expect(pwdInput).toBeDisabled();
+    getAllByRole('button').forEach((button) => {
+      expect(button).toBeDisabled();
+    });
+  });
 });
