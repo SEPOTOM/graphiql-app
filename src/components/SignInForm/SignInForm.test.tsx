b1f55ca6561fd4266ca6109a66135b9d4acd5b39
Artyom Pizin <artempetr2016@gmail.com>
2024-09-07 14:54:30 +0300

    test(SignInForm): add test for showing success message

--- src/components/SignInForm/SignInForm.test.tsx
+++ src/components/SignInForm/SignInForm.test.tsx
@@ -1,5 +1,8 @@
 import { render } from '@testing-library/react';
 
+import { renderWithUser } from '@/tests';
+import { AuthProvider } from '@/contexts';
+
 import SignInForm from './SignInForm';
 
 describe('SignInForm', () => {
@@ -8,5 +11,19 @@ describe('SignInForm', () => {
 
     expect(getByRole('textbox', { name: /email/i })).toBeInTheDocument();
     expect(getByLabelText(/^password/i)).toBeInTheDocument();
+  });
+
+  it('displays success alert after successful login', async () => {
+    const { user, getByRole, getByLabelText, findByRole } = renderWithUser(
+      <AuthProvider>
+        <SignInForm lng="en" />
+      </AuthProvider>
+    );
+
+    await user.type(getByRole('textbox', { name: /email/i }), 'mark@email.com');
+    await user.type(getByLabelText(/^password/i), 'mark12345678');
+    await user.click(getByRole('button', { name: /sign in/i }));
+
+    expect(await findByRole('alert')).toHaveTextContent(/success/i);
   });
 });
