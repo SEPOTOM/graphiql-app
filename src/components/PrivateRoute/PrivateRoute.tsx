7d78b52092b280fe0d3988482c4d92576cc3c628
Artyom Pizin <artempetr2016@gmail.com>
2024-09-09 18:57:30 +0300

    feat(PrivateRoute): add internationalization

--- src/components/PrivateRoute/PrivateRoute.tsx
+++ src/components/PrivateRoute/PrivateRoute.tsx
@@ -4,19 +4,22 @@ import { useRouter } from 'next/navigation';
 import { Box, CircularProgress, Typography } from '@mui/material';
 
 import { useAuth } from '@/contexts';
+import { useLanguage, useTranslation } from '@/hooks';
 
 import { PrivateRouteProps } from './types';
 
 const PrivateRoute = ({ children }: PrivateRouteProps) => {
   const { user, status } = useAuth();
   const router = useRouter();
+  const { lng } = useLanguage();
+  const { t } = useTranslation(lng);
 
   if (status === 'init') {
     return (
       <Box display="flex" justifyContent="center" alignItems="center" flexGrow={1}>
         <CircularProgress />
         <Typography variant="h4" component="p" ml={2}>
-          Authentication in progress...
+          {t('private_route.loading')}
         </Typography>
       </Box>
     );
@@ -28,7 +31,7 @@ const PrivateRoute = ({ children }: PrivateRouteProps) => {
     return (
       <Box display="flex" justifyContent="center" alignItems="center" flexGrow={1}>
         <Typography variant="h4" component="p">
-          Redirecting to homepage...
+          {t('private_route.redirect')}
         </Typography>
       </Box>
     );
