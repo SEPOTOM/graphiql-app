57ea808a17f47aa09bec32602892550eb9f2fbe3
Artyom Pizin <artempetr2016@gmail.com>
2024-09-10 15:07:39 +0300

    refactor(validation): reduce code duplication for auth forms schemas

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -3,30 +3,26 @@ import * as yup from 'yup';
 import { SignInFormData } from '@/components/SignInForm/types';
 import { SignUpFormData } from '@/components/SignUpForm/types';
 
-export const signInSchema: yup.ObjectSchema<SignInFormData> = yup.object().shape({
-  email: yup.string().required('Email is required').email('Invalid email address'),
+const emailSchema = yup.string().required('Email is required').email('Invalid email address');
 
-  password: yup
-    .string()
-    .required('Password is required')
-    .min(8, 'Password must be at least 8 characters')
-    .matches(/[a-zA-Z]/, 'Password must contain at least one letter')
-    .matches(/\d/, 'Password must contain at least one digit')
-    .matches(/[\p{P}\p{S}]/u, 'Password must contain at least one special character'),
+const passwordSchema = yup
+  .string()
+  .required('Password is required')
+  .min(8, 'Password must be at least 8 characters')
+  .matches(/[a-zA-Z]/, 'Password must contain at least one letter')
+  .matches(/\d/, 'Password must contain at least one digit')
+  .matches(/[\p{P}\p{S}]/u, 'Password must contain at least one special character');
+
+export const signInSchema: yup.ObjectSchema<SignInFormData> = yup.object().shape({
+  email: emailSchema,
+  password: passwordSchema,
 });
 
 export const signUpSchema: yup.ObjectSchema<SignUpFormData> = yup.object().shape({
   username: yup.string().required('Username is required'),
 
-  email: yup.string().required('Email is required').email('Invalid email address'),
-
-  password: yup
-    .string()
-    .required('Password is required')
-    .min(8, 'Password must be at least 8 characters')
-    .matches(/[a-zA-Z]/, 'Password must contain at least one letter')
-    .matches(/\d/, 'Password must contain at least one digit')
-    .matches(/[\p{P}\p{S}]/u, 'Password must contain at least one special character'),
+  email: emailSchema,
+  password: passwordSchema,
 
   confirmPassword: yup
     .string()
