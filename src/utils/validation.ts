9ee8bc3de99e9f3e66398629d614e81140c70db5
Artyom Pizin <artempetr2016@gmail.com>
2024-09-10 16:25:23 +0300

    feat(validation): add internationalization

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -4,17 +4,17 @@ import { SignInFormData, SignUpFormData } from '@/types';
 
 const emailSchema = yup
   .string()
-  .required('Email is required')
-  .matches(/^[^\s@]+@[^\s@]+\.[^\s@]+$/, 'Invalid email address')
-  .email('Invalid email address');
+  .required('email.required')
+  .matches(/^[^\s@]+@[^\s@]+\.[^\s@]+$/, 'email.invalid')
+  .email('email.invalid');
 
 const passwordSchema = yup
   .string()
-  .required('Password is required')
-  .min(8, 'Password must be at least 8 characters')
-  .matches(/[a-zA-Z]/, 'Password must contain at least one letter')
-  .matches(/\d/, 'Password must contain at least one digit')
-  .matches(/[\p{P}\p{S}]/u, 'Password must contain at least one special character');
+  .required('password.required')
+  .min(8, 'password.length')
+  .matches(/[a-zA-Z]/, 'password.letter')
+  .matches(/\d/, 'password.digit')
+  .matches(/[\p{P}\p{S}]/u, 'password.special_char');
 
 export const signInSchema: yup.ObjectSchema<SignInFormData> = yup.object().shape({
   email: emailSchema,
@@ -22,13 +22,13 @@ export const signInSchema: yup.ObjectSchema<SignInFormData> = yup.object().shape
 });
 
 export const signUpSchema: yup.ObjectSchema<SignUpFormData> = yup.object().shape({
-  username: yup.string().required('Username is required'),
+  username: yup.string().required('username.required'),
 
   email: emailSchema,
   password: passwordSchema,
 
   confirmPassword: yup
     .string()
-    .required('Confirm Password is required')
-    .oneOf([yup.ref('password')], 'Passwords must match'),
+    .required('confirm_pwd.required')
+    .oneOf([yup.ref('password')], 'confirm_pwd.match'),
 });
