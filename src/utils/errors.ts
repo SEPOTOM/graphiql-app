7ae23a832875581a3d75562f5ed9e9dfea3f4576
Artyom Pizin <artempetr2016@gmail.com>
2024-09-11 13:21:24 +0300

    feat(getAuthErrorMessage): handle invalid credential error

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -1,5 +1,8 @@
 export const getAuthErrorMessage = (errorCode: string) => {
   switch (errorCode) {
+    case 'auth/invalid-credential': {
+      return 'The credential provided is invalid.';
+    }
     default: {
       return 'An unexpected error occurred. Please try again.';
     }
