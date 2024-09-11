34ad228ae02a37aec2e70144944fc7b490a9dbea
Artyom Pizin <artempetr2016@gmail.com>
2024-09-11 19:08:31 +0300

    test(FormLayout): add test for handling invalid credential error

--- src/components/FormLayout/FormLayout.test.tsx
+++ src/components/FormLayout/FormLayout.test.tsx
@@ -28,4 +28,20 @@ describe('FormLayout', () => {
 
     expect(getByRole('alert')).toHaveTextContent(/email/i);
   });
+
+  it('should handle invalid credential error', async () => {
+    const handleSubmit = (e?: BaseSyntheticEvent) => {
+      e?.preventDefault();
+      throw new FirebaseError('auth/invalid-credential', 'The credential provided is invalid.');
+    };
+    const { user, getByRole } = renderWithUser(
+      <FormLayout {...sharedProps} onSubmit={handleSubmit}>
+        {children}
+      </FormLayout>
+    );
+
+    await user.click(getByRole('button', { name: 'Submit' }));
+
+    expect(getByRole('alert')).toHaveTextContent(/credential/i);
+  });
 });
