72fc7e866e6749bcde3cf28e094019432494a853
Artyom Pizin <artempetr2016@gmail.com>
2024-09-11 19:06:12 +0300

    test(FormLayout): add test for handling existing email error

---
+++ src/components/FormLayout/FormLayout.test.tsx
@@ -0,0 +1,31 @@
+import { BaseSyntheticEvent } from 'react';
+import { FirebaseError } from 'firebase/app';
+
+import { renderWithUser } from '@/tests';
+
+import FormLayout from './FormLayout';
+import { FormLayoutProps } from './types';
+
+const children = <button type="submit">Submit</button>;
+const sharedProps: Pick<FormLayoutProps, 'title' | 'lng'> = {
+  title: 'Test',
+  lng: 'en',
+};
+
+describe('FormLayout', () => {
+  it('should handle existing email error', async () => {
+    const handleSubmit = (e?: BaseSyntheticEvent) => {
+      e?.preventDefault();
+      throw new FirebaseError('auth/email-already-exists', 'The email is already exist.');
+    };
+    const { user, getByRole } = renderWithUser(
+      <FormLayout {...sharedProps} onSubmit={handleSubmit}>
+        {children}
+      </FormLayout>
+    );
+
+    await user.click(getByRole('button', { name: 'Submit' }));
+
+    expect(getByRole('alert')).toHaveTextContent(/email/i);
+  });
+});
