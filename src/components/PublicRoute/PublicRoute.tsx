739650f0408af85e97d302819a876aea6835de19
Artyom Pizin <artempetr2016@gmail.com>
2024-09-12 13:54:02 +0300

    feat(PublicRoute): add internationalization

--- src/components/PublicRoute/PublicRoute.tsx
+++ src/components/PublicRoute/PublicRoute.tsx
@@ -4,19 +4,22 @@ import { useRouter } from 'next/navigation';
 import { Box, CircularProgress, Typography } from '@mui/material';
 
 import { useAuth } from '@/contexts';
+import { useLanguage, useTranslation } from '@/hooks';
 
 import { PublicRouteProps } from './types';
 
 const PublicRoute = ({ children }: PublicRouteProps) => {
   const { status } = useAuth();
   const router = useRouter();
+  const { lng } = useLanguage();
+  const { t } = useTranslation(lng);
 
   if (status === 'authenticated') {
     router.replace('/');
     return (
       <Box display="flex" justifyContent="center" alignItems="center" flexGrow={1}>
         <Typography variant="h4" component="p">
-          Redirecting to the main page...
+          {t('public_route.redirect')}
         </Typography>
       </Box>
     );
@@ -30,7 +33,7 @@ const PublicRoute = ({ children }: PublicRouteProps) => {
     <Box display="flex" justifyContent="center" alignItems="center" flexGrow={1}>
       <CircularProgress />
       <Typography variant="h4" component="p" ml={2}>
-        Loading...
+        {t('public_route.loading')}
       </Typography>
     </Box>
   );
