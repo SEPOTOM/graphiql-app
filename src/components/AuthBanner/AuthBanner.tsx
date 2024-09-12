e8d52d1901d845875c76082f173dec1547c72309
Artyom Pizin <artempetr2016@gmail.com>
2024-09-12 23:09:38 +0300

    feat(AuthBanner): show links to private routes and updated welcome message for authenticated users

--- src/components/AuthBanner/AuthBanner.tsx
+++ src/components/AuthBanner/AuthBanner.tsx
@@ -1,31 +1,61 @@
+'use client';
+
 import Link from 'next/link';
 import { Button, List, ListItem, Typography } from '@mui/material';
 
+import { useAuth } from '@/contexts';
+
 const AuthBanner = () => {
+  const { user } = useAuth();
+
   return (
     <>
       <Typography variant="h2" component="h1" gutterBottom>
-        Welcome!
+        Welcome{user ? ` back, ${user.displayName}` : ''}!
       </Typography>
 
       <List
         sx={{
           display: 'flex',
           justifyContent: 'center',
+          alignItems: 'center',
           width: '100%',
         }}
       >
-        <ListItem sx={{ width: 'auto' }}>
-          <Button variant="contained" color="primary" component={Link} href="/sign-in" size="large">
-            Sign In
-          </Button>
-        </ListItem>
+        {user ?
+          <>
+            <ListItem>
+              <Button variant="contained" color="primary" component={Link} href="/GET" size="large" fullWidth>
+                REST Client
+              </Button>
+            </ListItem>
 
-        <ListItem sx={{ width: 'auto' }}>
-          <Button variant="outlined" color="primary" component={Link} href="/sign-up" size="large">
-            Sign Up
-          </Button>
-        </ListItem>
+            <ListItem>
+              <Button variant="contained" color="primary" component={Link} href="/GRAPHQL" size="large" fullWidth>
+                GraphiQL Client
+              </Button>
+            </ListItem>
+
+            <ListItem>
+              <Button variant="contained" color="primary" component={Link} href="/history" size="large" fullWidth>
+                History
+              </Button>
+            </ListItem>
+          </>
+        : <>
+            <ListItem sx={{ width: 'auto' }}>
+              <Button variant="contained" color="primary" component={Link} href="/sign-in" size="large">
+                Sign In
+              </Button>
+            </ListItem>
+
+            <ListItem sx={{ width: 'auto' }}>
+              <Button variant="outlined" color="primary" component={Link} href="/sign-up" size="large">
+                Sign Up
+              </Button>
+            </ListItem>
+          </>
+        }
       </List>
     </>
   );
