6ea4eff47c3778babaf1ca980c8219d074360107
Artyom Pizin <artempetr2016@gmail.com>
2024-09-12 14:24:28 +0300

    test(PublicRoute): add test for displaying nested content for unauthenticated users

--- src/components/PublicRoute/PublicRoute.test.tsx
+++ src/components/PublicRoute/PublicRoute.test.tsx
@@ -32,4 +32,16 @@ describe('PublicRoute', () => {
 
     expect(getByRole('paragraph')).toHaveTextContent(/redirect/i);
   });
+
+  it("shows the nested content is the user isn't authenticated", () => {
+    (useAuth as Mock).mockImplementation(() => ({ status: 'unauthenticated' }));
+
+    const { getByText } = render(
+      <LanguageProvider lang="en">
+        <PublicRoute>Test message</PublicRoute>
+      </LanguageProvider>
+    );
+
+    expect(getByText('Test message')).toBeInTheDocument();
+  });
 });
