718ff996e156c07870c0e68698b9368796d00875
Svetlana <svetailina1987@gmail.com>
2024-09-12 17:40:37 +0300

    feat: add EmptyRequestHistory

---
+++ src/components/HistorySection/EmptyRequestHistory/EmptyRequestHistory.tsx
@@ -0,0 +1,19 @@
+import { useLanguage, useTranslation } from '@/hooks';
+import { Box, Link, Typography } from '@mui/material';
+
+export default function EmptyRequestHistory() {
+  const { lng } = useLanguage();
+  const { t } = useTranslation(lng);
+
+  return (
+    <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 5 }}>
+      <Typography component="p" gutterBottom={true} variant="h4">
+        {t('empty_requests_history_message')}
+      </Typography>
+      <Box sx={{ width: '80%', display: 'flex', justifyContent: 'space-evenly', fontSize: 24 }}>
+        <Link href="/restfullClient/GET">{t('rest_client')}</Link>
+        <Link href="/GRAPHQL">{t('graphql_client')}</Link>
+      </Box>
+    </Box>
+  );
+}
