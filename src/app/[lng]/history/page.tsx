57486412af0cb389ba99fa171c69527789a21bb4
Svetlana <svetailina1987@gmail.com>
2024-09-13 18:09:45 +0300

    feat: add private route for history page

--- src/app/[lng]/history/page.tsx
+++ src/app/[lng]/history/page.tsx
@@ -1,10 +1,12 @@
-import { HistorySection } from '@/components';
+import { HistorySection, PrivateRoute } from '@/components';
 import { Container } from '@mui/material';
 
 export default function HistoryPage() {
   return (
-    <Container sx={{ flexGrow: '1', paddingBottom: '40px' }}>
-      <HistorySection />
-    </Container>
+    <PrivateRoute>
+      <Container sx={{ flexGrow: '1', paddingBottom: '40px' }}>
+        <HistorySection />
+      </Container>
+    </PrivateRoute>
   );
 }
