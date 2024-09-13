2f61b3bdf2d6ce6bb1d53d2faecbadaaf0672a98
Svetlana <svetailina1987@gmail.com>
2024-09-13 18:05:29 +0300

    fix: add translation

--- src/components/BodyEditor/RequestBodyEditor.tsx
+++ src/components/BodyEditor/RequestBodyEditor.tsx
@@ -84,7 +84,7 @@ export default function RequestBodyEditor({ mode, options, initialValue }: Reque
       if (e instanceof Error) {
         const newSegments = `${pathSegments.slice(0, SegmentIndex.Body).join('/')}?${params}`;
         window.history.replaceState(null, '', newSegments);
-        setErrorMessage(`Invalid JSON: ${e.message}`);
+        setErrorMessage(`${t('json_error')}${e.message}`);
         setShowErrorsPopover(true);
       }
     }
@@ -96,7 +96,7 @@ export default function RequestBodyEditor({ mode, options, initialValue }: Reque
     if (showErrorsPopover) {
       timer = setTimeout(() => {
         setShowErrorsPopover(false);
-      }, 2500);
+      }, 3000);
     }
     return () => {
       clearTimeout(timer);
