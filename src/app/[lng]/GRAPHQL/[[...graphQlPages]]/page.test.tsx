689aa96962586df29a53dde88d656ae7160ee463
Artyom Pizin <artempetr2016@gmail.com>
2024-09-15 02:40:33 +0300

    test(GraphQlClientPage): use renderWithLng to simplify tests

--- src/app/[lng]/GRAPHQL/[[...graphQlPages]]/page.test.tsx
+++ src/app/[lng]/GRAPHQL/[[...graphQlPages]]/page.test.tsx
@@ -1,15 +1,17 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { Mock } from 'vitest';
 import GraphQlClientPage from './page';
-import { GraphQlDataProvider, LanguageProvider } from '@/contexts';
+import { GraphQlDataProvider } from '@/contexts';
 import { ReactNode } from 'react';
+import { renderWithLng } from '@/tests';
 
 vi.mock('@/components/PrivateRoute/PrivateRoute', () => ({
   default: ({ children }: { children: ReactNode }) => {
     return children;
   },
 }));
+
 describe('GraphQlClientPage', () => {
   it('should  render page correctly', () => {
     (usePathname as Mock).mockReturnValue('GRAPHQL/en');
@@ -20,13 +22,12 @@ describe('GraphQlClientPage', () => {
     const mockedSearch = new URLSearchParams();
     (useSearchParams as Mock).mockReturnValue(mockedSearch);
 
-    render(
+    renderWithLng(
       <GraphQlDataProvider>
-        <LanguageProvider lang="en">
-          <GraphQlClientPage />
-        </LanguageProvider>
+        <GraphQlClientPage />
       </GraphQlDataProvider>
     );
+
     expect(screen.getByLabelText('Endpoint URL')).toBeInTheDocument();
     expect(screen.getByLabelText('SDL URL')).toBeInTheDocument();
   });
