f0c487898659e4ac76a3d2588d446d29dc0c3b03
Artyom Pizin <artempetr2016@gmail.com>
2024-09-15 02:42:16 +0300

    test(ErrorPage): use renderWithLng to simplify tests

--- src/app/[lng]/error.test.tsx
+++ src/app/[lng]/error.test.tsx
@@ -1,16 +1,10 @@
-import { render } from '@testing-library/react';
-
-import { LanguageProvider } from '@/contexts';
+import { renderWithLng } from '@/tests';
 
 import ErrorPage from './error';
 
 describe('ErrorPage', () => {
   it('displays relevant content', async () => {
-    const { findByRole, getByRole } = render(
-      <LanguageProvider lang="en">
-        <ErrorPage />
-      </LanguageProvider>
-    );
+    const { findByRole, getByRole } = renderWithLng(<ErrorPage />);
 
     expect(await findByRole('heading', { name: /something went wrong/i })).toBeInTheDocument();
     expect(getByRole('link', { name: /home/i })).toBeInTheDocument();
