6ebd319b00937c4ab53645c39f0a79c38557d0bb
Artyom Pizin <artempetr2016@gmail.com>
2024-09-15 03:05:25 +0300

    test(RequestBody): simplify tests with custom render functions

--- src/components/RequestBody/BodyModeToggle/RequestBodyToggle.test.tsx
+++ src/components/RequestBody/BodyModeToggle/RequestBodyToggle.test.tsx
@@ -1,9 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { screen } from '@testing-library/react';
 import RequestBodyToggle, { RequestBodyToggleProps } from './RequestBodyToggle';
 import { usePathname } from 'next/navigation';
 import { Mock } from 'vitest';
-import { LanguageProvider } from '@/contexts';
+import { renderWithUserAndLng } from '@/tests';
 
 describe('RequestBodyTypeSelector', () => {
   const setup = (props: Partial<RequestBodyToggleProps> = {}) => {
@@ -13,19 +12,14 @@ describe('RequestBodyTypeSelector', () => {
       ...props,
     };
 
-    return render(
-      <LanguageProvider lang="en">
-        <RequestBodyToggle {...defaultProps} />
-      </LanguageProvider>
-    );
+    return renderWithUserAndLng(<RequestBodyToggle {...defaultProps} />);
   };
 
   (usePathname as Mock).mockReturnValue('/restfullClient/en/');
 
   it('calls handleChange when a new mode is selected', async () => {
     const handleChange = vi.fn();
-    const user = userEvent.setup();
-    setup({ handleChange });
+    const { user } = setup({ handleChange });
 
     await user.click(screen.getByLabelText('raw request body'));
     expect(handleChange).toHaveBeenCalledTimes(1);
--- src/components/RequestBody/RequestBody.test.tsx
+++ src/components/RequestBody/RequestBody.test.tsx
@@ -1,22 +1,14 @@
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import RequestBody from './RequestBody';
-import userEvent from '@testing-library/user-event';
 import { usePathname, useSearchParams } from 'next/navigation';
 import { Mock } from 'vitest';
-import { LanguageProvider } from '@/contexts';
-
-const user = userEvent.setup();
+import { renderWithUserAndLng } from '@/tests';
 
 describe('RequestBody Component', () => {
   it('displays the RequestBodyTypeSelector when bodyType is set to "raw"', async () => {
     (usePathname as Mock).mockReturnValue('/restfullClient');
     (useSearchParams as Mock).mockReturnValue(new URLSearchParams());
-
-    render(
-      <LanguageProvider lang="en">
-        <RequestBody />
-      </LanguageProvider>
-    );
+    const { user } = renderWithUserAndLng(<RequestBody />);
 
     await user.click(screen.getByLabelText('raw request body'));
 
